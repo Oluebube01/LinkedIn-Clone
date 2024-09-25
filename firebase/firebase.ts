535e24708bf53535e5825103bc9aa7c1d0ad5e63
Oluebube01 <oluebube.madu@yahoo.com>
2024-09-25 21:36:06 +0100

    updatin files and fixing bugs

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -4,8 +4,30 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const fs = require('fs');
+const yaml = require('js-yaml');
+const dotenv = require('dotenv');
+
+// Function to load YAML and set environment variables
+function loadSecrets() {
+try {
+     // Read the secret.yml file
+    const secrets = yaml.load(fs.readFileSync('secret.yml', 'utf8'));
+    
+     // Set the API key as an environment variable
+    process.env.API_KEY = secrets.API_KEY;
+
+    console.log('Secrets loaded and API key set to environment variables');
+} catch (error) {
+    console.error('Error loading secrets:', error);
+}
+}
+
+// Call the function to load the secrets
+loadSecrets();
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
